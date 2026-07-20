f0bf170abb59fe8f86e16884575405ed38c62cf8
CYH-SWU <3205661352@qq.com>
2026-07-20 18:12:34 +0000

    test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,13 +1,32 @@
-"""测试所有关键模块能否正常导入"""
+"""Test that all key modules can be imported correctly."""
+
+from env.g1_env import G1Env
+from env.utils.config import G1EnvConfig
+from rl.policy import AsymmetricPolicy
+from rl.callbacks import AdaptiveLRScheduleCallback, CurriculumCallback
+from env.utils.step_sequence import WalkModes, StepSequenceGenerator
+from env.utils.observation_builder import ObservationBuilder
+from env.utils.reward_calculator import RewardCalculator
+from env.utils.terrain_generator import TerrainGenerator
 
 
 def test_import_env():
-    assert True
+    """Test env module imports."""
+    assert G1Env is not None
+    assert G1EnvConfig is not None
 
 
 def test_import_rl():
-    assert True
+    """Test rl module imports."""
+    assert AsymmetricPolicy is not None
+    assert AdaptiveLRScheduleCallback is not None
+    assert CurriculumCallback is not None
 
 
 def test_import_utils():
-    assert True
+    """Test env.utils module imports."""
+    assert WalkModes is not None
+    assert StepSequenceGenerator is not None
+    assert ObservationBuilder is not None
+    assert RewardCalculator is not None
+    assert TerrainGenerator is not None
